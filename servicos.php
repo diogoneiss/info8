02b445a7f2c20b5bf64702689dfe10538a72636f
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2020-03-30 15:59:10 -0300

    Update servicos.php

--- servicos.php
+++ servicos.php
@@ -1,82 +1,49 @@
             <!-- service Section -->
             <section id="service" class="service">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="main_service">
-                            <div class="col-md-6 col-sm-12 no-padding">
+            	<div class="container-fluid">
+            		<div class="row">
+            			<div class="main_service">
+            				<div class="col-md-6 col-sm-12 no-padding">
 
-                                <div class="single_service single_service_text text-right">
-                                    <div class="head_title">
-                                        <h2>NOSSOS SERVIÇOS</h2>
-                                    </div>
+            					<div class="single_service single_service_text text-right">
+            						<div class="head_title">
+            							<h2>NOSSOS SERVIÇOS</h2>
+            						</div>
 
-                                    <div class="row">
-                                        <div class="col-md-12 col-sm-10 col-xs-10 margin-bottom-60">
-                                            <div class="row">
+            						<div class="row">
+            							<div class="col-md-12 col-sm-10 col-xs-10 margin-bottom-60">
+            								<div class="row">
 
-                                                <div class="col-sm-10 col-sm-offset-1 col-xs-9 col-xs-offset-1">
-                                                    <article class="single_service_right_text">
-                                                        <h4>Past8</h4>
-                                                        <p>Sistema de controle para profissionais da saúde e seus respectivos pacientes.
-</p>
-                                                    </article>
-                                                </div>
-                                                <div class="col-sm-1 col-xs-1">
-                                                    <figure class="single_service_icon">
-                                                        <center>   <img src="<?php bloginfo('template_url'); ?>/assets/images/p8.png"  height="45px"></center>
-                                                    </figure><!-- End of figure -->
-                                                </div>
-                                            </div>
-                                        </div><!-- End of col-sm-12 -->
+            									<div class="col-sm-10 col-sm-offset-1 col-xs-9 col-xs-offset-1">
+            										<article class="single_service_right_text">
+            											<h4>Past8</h4>
+            											<p>Sistema de controle para profissionais da saúde e seus respectivos
+            												pacientes.
+            											</p>
+            										</article>
+            									</div>
+            									<div class="col-sm-1 col-xs-1">
+            										<figure class="single_service_icon">
+            											<center> <img src="<?php bloginfo('template_url'); ?>/assets/images/p8.png"
+            													height="45px"></center>
+            										</figure><!-- End of figure -->
+            									</div>
+            								</div>
+            							</div><!-- End of col-sm-12 -->
 
-                                        <div class="col-md-12 col-sm-10 col-xs-10 margin-bottom-60">
-                                            <div class="row">
+            						</div>
+            					</div>
+            				</div><!-- End of col-sm-6 -->
 
-                                                <div class="col-sm-10 col-sm-offset-1 col-xs-9 col-xs-offset-1">
-                                                    <article class="single_service_right_text">
-                                                        <h4>Ark8</h4>
-                                                        <p>Sistema de controle pessoal.
-</p>
-                                                    </article>
-                                                </div>
-                                                <div class="col-sm-1 col-xs-1">
-                                                    <figure class="single_service_icon">
-                                                        <center>   <img src="<?php bloginfo('template_url'); ?>/assets/images/a8.png"  height="45px"></center>
-                                                    </figure><!-- End of figure -->
-                                                </div>
-                                            </div>
-                                        </div><!-- End of col-sm-12 -->
+            				<div class="col-md-6 col-sm-12 no-padding">
+            					<figure class="single_service single_service_img">
+            						<div class="overlay-img"></div>
+            						<img src="<?php bloginfo('template_url'); ?>/assets/images/site/Produtos-filtro.jpg"
+            							alt="" />
+            					</figure><!-- End of figure -->
+            				</div><!-- End of col-sm-6 -->
 
-                                        <div class="col-md-12 col-sm-10 col-xs-10 margin-bottom-60">
-                                            <div class="row">
-
-                                                <div class="col-sm-10 col-sm-offset-1 col-xs-9 col-xs-offset-1 margin-bottom-20">
-                                                    <article class="single_service_right_text">
-                                                        <h4>Marcha8</h4>
-                                                        <p>Sistema de controle para animais.
-</p>
-                                                    </article>
-                                                </div>
-                                                <div class="col-sm-1 col-xs-1">
-                                                    <figure class="single_service_icon">
-                                                        <center>   <img src="<?php bloginfo('template_url'); ?>/assets/images/m8.png"  height="45px"></center>
-                                                    </figure><!-- End of figure -->
-                                                </div>
-                                            </div>
-                                        </div><!-- End of col-sm-12 -->
-
-                                    </div>
-                                </div>
-                            </div><!-- End of col-sm-6 -->
-
-                            <div class="col-md-6 col-sm-12 no-padding">
-                                <figure class="single_service single_service_img">
-                                    <div class="overlay-img"></div>
-                                    <img src="<?php bloginfo('template_url'); ?>/assets/images/site/Produtos-filtro.jpg" alt="" />
-                                </figure><!-- End of figure -->
-                            </div><!-- End of col-sm-6 -->
-
-                        </div>
-                    </div><!-- End of row -->
-                </div><!-- End of Container-fluid -->
+            			</div>
+            		</div><!-- End of row -->
+            	</div><!-- End of Container-fluid -->
             </section><!-- End of service Section -->
