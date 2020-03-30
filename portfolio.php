c1d6ba42ff2fd31345f80ce5ba502c159cac64d3
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2020-03-30 16:19:14 -0300

    Create portfolio.php

---
+++ portfolio.php
@@ -0,0 +1,120 @@
+
+            <section id="portfolio" class="portfolio sections" style="">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="main_portfolio">
+                            <div class="col-sm-12">
+                                <div class="head_title text-center">
+                                    <h2>Nossos Parceiros</h2>
+                                    
+                                    <div class="separator"></div>
+                                </div>
+                            </div>
+                            <div class="work_menu text-center" style="display:none">
+                                <div id="filters" class="toolbar mb2 mt2">
+                                    <button class="btn-md fil-cat filter active" data-filter="all">ALL</button>/
+                                    <button class="btn-md fil-cat filter" data-rel="web" data-filter=".web">WEB DESIGN</button>/
+                                    <button class="btn-md fil-cat filter" data-rel="design" data-filter=".design">PRINT DESIGN</button>/
+                                    <button class="btn-md fil-cat filter" data-rel="flyers" data-filter=".flyers">ANIMATION</button>/
+                                    <button class="btn-md fil-cat filter" data-rel="bcards" data-filter=".bcards">ART</button>/
+                                    <button class="btn-md fil-cat filter" data-rel="photo" data-filter=".photo">PHOTOGRAPHY</button>/
+                                    <button class="btn-md fil-cat filter" data-rel="video" data-filter=".video">VIDEO</button>
+                                </div> 
+
+                            </div>
+
+                            <div style="clear:both;"></div>     
+                            <div id="portfoliowork"> 
+                                <div class="single_portfolio tile scale-anm web grid-item-width2 video" >
+                                    <img src="<?php bloginfo('template_url'); ?>/assets/images/t01.jpg" alt="" />
+                                    <a href="<?php bloginfo('template_url'); ?>/assets/images/t01.jpg" class="portfolio-img">
+                                        <div class="grid_item_overlay">
+                                            <div class="separator4"> <h3>ARK8</h3></div>
+                                           
+                                         
+                                        </div>
+                                    </a>
+                                </div>
+								  <div class="single_portfolio tile scale-anm web grid-item-width2 video" >
+                                  <img src="<?php bloginfo('template_url'); ?>/assets/images/t02.jpg" alt="" />
+                                    <a href="<?php bloginfo('template_url'); ?>/assets/images/t02.jpg" class="portfolio-img">
+                                        <div class="grid_item_overlay">
+                                            <div class="separator4"> <h3>Mieloma</h3></div>
+                                           
+                                           
+                                        </div>
+                                    </a>
+                                </div>
+								  <div class="single_portfolio tile scale-anm web grid-item-width2 video" >
+                                <img src="<?php bloginfo('template_url'); ?>/assets/images/t03.jpg" alt="" />
+                                    <a href="<?php bloginfo('template_url'); ?>/assets/images/t03.jpg" class="portfolio-img">
+                                        <div class="grid_item_overlay">
+                                            <div class="separator4"> <h3>Linfoma</h3></div>
+                                           
+                                         
+                                        </div>
+                                    </a>
+                                </div>
+								  <div class="single_portfolio tile scale-anm web grid-item-width2 video" >
+                      <img src="<?php bloginfo('template_url'); ?>/assets/images/t04.jpg" alt="" />
+                                    <a href="<?php bloginfo('template_url'); ?>/assets/images/t04.jpg" class="portfolio-img">
+                                        <div class="grid_item_overlay">
+                                            <div class="separator4">   <h3>Marcha8</h3></div>
+                                         
+                                          
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="single_portfolio tile scale-anm bcards photo" >
+                            <img src="<?php bloginfo('template_url'); ?>/assets/images/t05.jpg" alt="" />
+                                    <a href="<?php bloginfo('template_url'); ?>/assets/images/t05.jpg" class="portfolio-img">
+                                        <div class="grid_item_overlay">
+                                            <div class="separator4"><h3>Linfoma</h3></div>
+                                            
+                                         
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="single_portfolio tile scale-anm web video">
+                              <img src="<?php bloginfo('template_url'); ?>/assets/images/t06.jpg" alt="" />
+                                    <a href="<?php bloginfo('template_url'); ?>/assets/images/t06.jpg" class="portfolio-img">
+                                 
+                                        <div class="grid_item_overlay">
+                                            <div class="separator4"><h3>Leucemia</h3></div>
+                                            
+                                         
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="single_portfolio tile scale-anm web photo" >
+                             <img src="<?php bloginfo('template_url'); ?>/assets/images/t07.jpg" alt="" />
+                                    <a href="<?php bloginfo('template_url'); ?>/assets/images/t07.jpg" class="portfolio-img">
+                                        <div class="grid_item_overlay">
+                                            <div class="separator4">  <h3>Diabetes</h3></div>
+                                          
+                                           
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="single_portfolio tile scale-anm bcards design" >
+                                 <img src="<?php bloginfo('template_url'); ?>/assets/images/t08.jpg" alt="" />
+                                    <a href="<?php bloginfo('template_url'); ?>/assets/images/t08.jpg" class="portfolio-img">
+                                        <div class="grid_item_overlay">
+                                            <div class="separator4"> <h3>Past8</h3></div>
+                                           
+                                        
+                                        </div>
+                                    </a>
+                                </div>
+
+                         
+                          
+
+
+                            </div>
+
+                            <div style="clear:both;"></div>  
+                        </div>
+                    </div>
+                </div><!-- End off container --> 
+            </section> <!-- End off Work Section -->            
