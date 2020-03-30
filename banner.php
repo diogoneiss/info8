691e749dffcde4fc5ac1c4c534cc376e84ebf848
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2020-03-30 15:48:22 -0300

    Remoçãodo slideshow

--- banner.php
+++ banner.php
@@ -1,57 +1,36 @@
      <!--home Section -->
-            <section id="home" class="home">
-                <div class="overlay">
-                    <div class="home_skew_border">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-sm-12 ">
-                                    <div class="main_home_slider text-center">
-                                        <div class="single_home_slider">
-                                            <div class="main_home wow fadeInUp" data-wow-duration="700ms">
-                                               
-                                                <h1>Médico ou Paciente ?</h1>
-                                                <div class="separator"></div>
-                                                <p>Exames, receitas, consultas, prontuários e muito mais em um só click. Tenha em mãos todo o seu histórico nas nuvens, com arquivos em JPEG (imagens) e PDF. O Past8 trará comodidade, tranquilidade e segurança para a sua vida.    E tudo totalmente de graça;</p>
-                                                <div class="home_btn">
-                                                    <a href="" class="btn btn-lg m_t_10">Comece a usar agora</a>
-                                                    <a href="" class="btn btn-default">Quer saber mais</a>
-                                                </div>
+     <section id="home" class="home">
+     	<div class="overlay">
+     		<div class="home_skew_border">
+     			<div class="container">
+     				<div class="row">
+     					<div class="col-sm-12 ">
+     						<div class="main_home_slider text-center">
+     							<div class="single_home_slider">
+     								<div class="main_home wow fadeInUp" data-wow-duration="700ms">
 
-                                            </div>
-                                        </div>
-                                        <div class="single_home_slider">
-                                            <div class="main_home wow fadeInUp" data-wow-duration="700ms">
-                                              
-                                                <h1>Sabe o que fazer com tanto papel ?</h1>
-                                                <div class="separator"></div>
-                                                <p>Contas, notas fiscais, manuais, receitas, e muito mais em um só click.Tenha em mãos fotos dos arquivos da forma que escolher de onde estiver.O Ark8 ajudará a organizar a sua vida financeira, acadêmica e até o seu lazer.</p>
-                                                <div class="home_btn">
-                                                    <a href="" class="btn btn-lg m_t_10">Comece a usar agora</a>
-                                                    <a href="" class="btn btn-default">Quer saber mais</a>
-                                                </div>
+     									<h1>Médico ou Paciente ?</h1>
+     									<div class="separator"></div>
+     									<p>Exames, receitas, consultas, prontuários e muito mais em um só click. Tenha em mãos
+     										todo o seu histórico nas nuvens, com arquivos em JPEG (imagens) e PDF. O Past8 trará
+     										comodidade, tranquilidade e segurança para a sua vida. E tudo totalmente de graça;
+     									</p>
+     									<div class="home_btn">
+     										<a href="" class="btn btn-lg m_t_10">Comece a usar agora</a>
+     										<a href="" class="btn btn-default">Quer saber mais</a>
+     									</div>
 
-                                            </div>
-                                        </div>
-                                        <div class="single_home_slider">
-                                            <div class="main_home wow fadeInUp" data-wow-duration="700ms">
-                                           
-                                                <h1>Vamos organizar sua fazenda?</h1>
-                                                <div class="separator"></div>
-                                                <p>Cavalos, Vacas, Cachorros, Pássaros e qualquer outro animal que possuir criação em grande quantidade, terão todos os nomes, exames, vacinas, painel genético e muito mais em um só lugar. Acesse de onde estiver, seja na cidade ou em sua fazenda! O Marcha8 trará segurança e tranquilidade para cuidar dos seus animais.</p>
-                                                <div class="home_btn">
-                                                      <a href="" class="btn btn-lg m_t_10">Comece a usar agora</a>
-                                                    <a href="" class="btn btn-default">Quer saber mais</a>
-                                                </div>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="scrooldown">
-                            <a href="#feature"><i class="fa fa-arrow-down"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </section><!--End of home section -->
+     								</div>
+     							</div>
+     						
+        					</div>
+     					</div>
+     				</div>
+     			</div>
+     			<div class="scrooldown">
+     				<a href="#feature"><i class="fa fa-arrow-down"></i></a>
+     			</div>
+     		</div>
+     	</div>
+     </section>
+     <!--End of home section -->
