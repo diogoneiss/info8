30b41359634c54fd7e958a6e9ca602ea43cfd749
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2020-04-08 17:32:35 -0300

    Create precos.php

---
+++ precos.php
@@ -0,0 +1,75 @@
+            <!-- Pricing Section -->
+            <section id="pricing" class="pricing">
+                <div class="container">
+                    <div class="row">
+                        <div class="main_pricing_area sections">
+                            <div class="head_title text-center">
+                                <h2>PRODUTOS</h2>
+
+                                <div class="separator"></div>
+                            </div><!-- End off Head_title -->
+
+                            <div class="col-md-4 col-sm-6">
+                                <div class="single_pricing">
+                                    <div class="pricing_head">
+                                        <h3>ARK8</h3>
+                                        <div class="pricing_price">
+                                            <div class="p_r text-center"><img src="<?php bloginfo('template_url'); ?>/assets/images/a8.png"  height="35px"></div>
+                                          
+                                        </div>
+                                    </div>
+
+                                    <div class="pricing_body">
+                                        <ul>
+                                            <li>Sistema de controle pessoal.
+
+</li>
+                                      
+                                         
+                                        </ul>
+                                        <a href="" class="roxo btn btn-md">SAIBA MAIS</a>
+                                    </div>
+                                </div>
+                            </div> <!-- End off col-md-4 -->
+                            <div class="col-md-4 col-sm-6">
+                                <div class="single_pricing pricing_business">
+                                    <div class="pricing_head ">
+                                        <h3>PAST8</h3>
+                                           <div class="pricing_price">
+                                            <div class="p_r text-center"><img src="<?php bloginfo('template_url'); ?>/assets/images/p8.png"  height="35px"></div>
+                                          
+                                        </div>
+                                    </div>
+
+                                    <div class="pricing_body">
+                                        <ul>
+                                             <li>Controle para Pacientes</li>
+                                            <li>Controle para Profissionais da saúde.</li>
+                                        </ul>
+                                       <a href="" class="vermelho btn btn-md">SAIBA MAIS</a>
+                                    </div>
+                                </div>
+                            </div> <!-- End off col-md-4 -->
+                            <div class="col-md-4 col-sm-6">
+                                <div class="single_pricing precomar">
+                                    <div class="pricing_head">
+                                        <h3>MARCHA8</h3>
+                                           <div class="pricing_price">
+                                            <div class="p_r text-center"><img src="<?php bloginfo('template_url'); ?>/assets/images/m8.png"  height="35px"></div>
+                                          
+                                        </div>
+                                    </div>
+
+                                    <div class="pricing_body">
+                                        <ul>
+                                            <li>Sistema de controle para animais.
+</li>
+                                        </ul>
+                                       <a href="" class="verde btn btn-md">SAIBA MAIS</a>
+                                    </div>
+                                </div>
+                            </div> <!-- End off col-md-4 -->
+                        </div>
+                    </div><!-- End off row -->
+                </div><!-- End off container -->
+            </section><!-- End off Pricing Section -->
