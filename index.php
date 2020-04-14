66bda9599ca6547df6ca43e86510c5c5ccced99c
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2020-04-14 16:06:37 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php get_header();?>
+<?php  include (TEMPLATEPATH . '/banner.php'); ?>
+
+<section id="consulta" style="margin: 0 3% !important; width: 75% !important; margin-top: 3% !important; " class="trial wow fadeIn"  data-wow-duration="2s" data-wow-dealy="1.5s">
+	<div class="jumbotron jumbotron-fluid" style="padding: 5% !important;">
+  <h1 class="display-4" style="margin-bottom: 1em !important;">Atendimento médico online, gratuito</h1>
+  <p class="lead">Precisa de orientação médica a respeito de sua saúde?</p>
+  <hr class="my-4">
+  <p>Reunimos médicos voluntários, dispostos a ajudar pacientes necessitados, remotamente. Durante a pandemia do COVID-19 o procedimento é totalmente gratuito. Clique no botão abaixo para começar</p>
+  <a class="btn btn-primary btn-lg" href="https://forms.gle/Etyyxd99ozgatAb1A" role="button">Começar</a>
+</div>
+</section>
+
+<?php  include (TEMPLATEPATH . '/diferenciais.php'); ?>   
+<?php  include (TEMPLATEPATH . '/historias.php'); ?>   
+<?php  include (TEMPLATEPATH . '/servicos.php'); ?>   
+<?php  include (TEMPLATEPATH . '/portfolio.php'); ?>   
+<?php  include (TEMPLATEPATH . '/depoimentos.php'); ?>   
+<?php  include (TEMPLATEPATH . '/counter.php'); ?>    
+<?php  include (TEMPLATEPATH . '/precos.php'); ?>        
+<?php  include (TEMPLATEPATH . '/equipe.php'); ?> 
+
+<?php  include (TEMPLATEPATH . '/clientes.php'); ?> 
+
+<?php  //include (TEMPLATEPATH . '/blog.php'); ?> 
+<?php  include (TEMPLATEPATH . '/contato.php'); ?>       
+<section id="trial" class="trial text-center wow fadeIn"  data-wow-duration="2s" data-wow-dealy="1.5s">
+                <div class="main_trial_area">
+                    <div class="video_overlay sections">
+                        <div class="container">
+                            <div class="row">
+                                <div class="main_trial">
+                                    <div class="col-sm-12">
+                                        <h2>EXPERIMENTE O PAST8. É TOTALMENTE <span>GRÁTIS!</span></h2>
+                                      
+                                        <a href="" class="btn btn-lg">Começar Usar Agora</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+</section><!-- End of Trial section -->
+
+	<?php
+
+get_footer(); 
+
+
+
+?>
